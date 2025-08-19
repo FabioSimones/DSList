f542303ddf6357b6b9a4031471135f1aaf2512cb
Fábio simones <simones.fabio1994@gmail.com>
2025-08-19 18:44:09 -0300

    Buscando Game por ID.

--- src/main/java/dev/fabiosimones/dslist/controllers/GameController.java
+++ src/main/java/dev/fabiosimones/dslist/controllers/GameController.java
@@ -1,10 +1,12 @@
 package dev.fabiosimones.dslist.controllers;
 
+import dev.fabiosimones.dslist.dto.GameDTO;
 import dev.fabiosimones.dslist.dto.GameMinDTO;
 import dev.fabiosimones.dslist.entities.Game;
 import dev.fabiosimones.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +18,11 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        return gameService.findById(id);
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll(){
